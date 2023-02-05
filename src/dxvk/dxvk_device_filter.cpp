f27f66832378aa15962197f0de6c76723c9dbc8e
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2023-02-04 20:22:57 -0800

    [dxvk] Add DXVK_MIN_VK_VERSION
    Allows us to force run with haswell/ivy bridge intel chips that don't support 1.3. DO NOT UPSTREAM THIS

--- src/dxvk/dxvk_device_filter.cpp
+++ src/dxvk/dxvk_device_filter.cpp
@@ -1,5 +1,7 @@
 #include "dxvk_device_filter.h"
 
+#include <cstdio>
+
 namespace dxvk {
   
   DxvkDeviceFilter::DxvkDeviceFilter(DxvkDeviceFilterFlags flags)
@@ -14,10 +16,25 @@ namespace dxvk {
   DxvkDeviceFilter::~DxvkDeviceFilter() {
     
   }
-  
+
+  // returns 1.3.0 or whatever the user decides to override us with
+  static auto getMinVersion() {
+    static const auto  ver = []() {
+      auto minVer = env::getEnvVar("DXVK_MIN_VK_VERSION");
+      int major = 1, minor = 3, patch = 0;
+      if (minVer.length() > 0) {
+        int n = std::sscanf(minVer.c_str(), "%d.%d.%d", &major, &minor, &patch);
+        major = (n>0) ? major : 1;
+        minor = (n>1) ? minor : 3;
+        patch = (n>2) ? patch : 0;
+      }
+      return VK_MAKE_VERSION(major, minor, patch);
+    }();
+    return ver;
+  }
   
   bool DxvkDeviceFilter::testAdapter(const VkPhysicalDeviceProperties& properties) const {
-    if (properties.apiVersion < VK_MAKE_VERSION(1, 3, 0)) {
+    if (properties.apiVersion < getMinVersion()) {
       Logger::warn(str::format("Skipping Vulkan ",
         VK_VERSION_MAJOR(properties.apiVersion), ".",
         VK_VERSION_MINOR(properties.apiVersion), " adapter: ",
